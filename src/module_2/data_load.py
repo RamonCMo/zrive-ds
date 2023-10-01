e19c522ec32628da60ecf7eee668321539a3ba6a
Ramon Coronado <ramoncormo8@gmail.com>
2023-10-01 17:13:14 +0200

    data checks

--- src/module_2/data_load.py
+++ src/module_2/data_load.py
@@ -1,18 +1,23 @@
-import boto3 
+import boto3
 import os
 
-BUCKET_NAME = 'zrive-ds-data'
-prefix = 'groceries/sampled-datasets/' # helps to search particular folder in bucket
-session = boto3.Session(profile_name='default')
-s3 = session.resource('s3')
+BUCKET_NAME = "zrive-ds-data"
+prefix = "groceries/sampled-datasets/"  # helps to search particular folder in bucket
+session = boto3.Session(profile_name="default")
+s3 = session.resource("s3")
+
 
 def download_folder(bucket_name, s3_folder, local_dir=None):
     bucket = s3.Bucket(bucket_name)
     for obj in bucket.objects.filter(Prefix=s3_folder):
-        target = obj.key if local_dir is None \
+        target = (
+            obj.key
+            if local_dir is None
             else os.path.join(local_dir, os.path.relpath(obj.key, s3_folder))
+        )
         if not os.path.exists(os.path.dirname(target)):
             os.makedirs(os.path.dirname(target))
         bucket.download_file(obj.key, target)
 
-download_folder(BUCKET_NAME, prefix, 'data/')
+
+download_folder(BUCKET_NAME, prefix, "data/")
